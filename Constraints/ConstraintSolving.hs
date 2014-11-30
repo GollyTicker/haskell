a917dd0c2ab76b948b9eb800d139a1ac74b0ff15
GollyTicker <golly.ticker@gmail.com>
2014-11-30 15:51:19 +0100

    working with two types

--- Constraints/ConstraintSolving.hs
+++ Constraints/ConstraintSolving.hs
@@ -4,7 +4,9 @@
 module ConstraintSolving (
         Net(..), Constraint, Node,
         Domain, NodeName,
-        var, mkConstraint, Elem, Over, On
+        var, mkConstraint, Elem, Over, On,
+        ac1,
+        Typeable
     )
     where
 
@@ -17,7 +19,7 @@ import Data.Typeable
 
 -- Ein Knoten besteht aus seinem Namen sowie aus dessen Domain an besetzbaren Werten
 data Node = forall a. Elem a =>
-                Node NodeName (Domain a)
+                Node !NodeName (Domain a)
 
 var :: Elem a => NodeName -> Domain a -> Node
 var = Node
@@ -32,7 +34,9 @@ instance Show Node where
     show (Node s ls) = "Node " ++ s ++ " " ++ show ls
 
 -- Ein Constraint Netz besteht aus den Knoten und den Constraints
-data Net = Net [Node] [Constraint] deriving Show
+data Net = Net [Node] ![Constraint]
+instance Show Net where
+    show (Net ns cs) = "Net \n  " ++ show ns ++ "\n  " ++ show cs
 
 {-
 Ein Type synonym für Show und Typeable class.
@@ -49,7 +53,7 @@ data Constraint =
             NodeName -- Name of first Node
             NodeName -- Name of second Node
             (Node -> Node -> (Node, Bool))
-                -- funciton to run the constraint on two arbitrary nodes.
+                -- function to run the constraint on two arbitrary nodes.
                 -- if it returns false, then the constraint was violated.
             (a -> b -> Bool)
                 -- a copy of the original constrain on the original type
@@ -79,8 +83,7 @@ mkConstraint n1 f n2 s = Binary s n1 n2 g f
         g xNode@(Node x' xs) (Node y' ys)
             | x' == n1 && y' == n2 =
                 let xs' :: Domain a
-                    xs' = fromJust . cast
-                            $ [ x | x <- xs, any (\y -> x `comp` y) ys]
+                    xs' = fromJust $ cast [ x | x <- xs, any (\y -> x `comp` y) ys]
                 in ( Node x' xs', (length xs') /= length (xs) )
             | otherwise = (xNode, False)
 
@@ -94,42 +97,37 @@ type On a = (Elem a) =>
                 NodeName -> (a -> a -> Bool) -> NodeName
                 -> String -> Constraint
 
-
-{-arcconsistency1 :: forall a. (Eq a, Show a) => Net a -> IO (Net a)
-arcconsistency1 net@(Net ns cs) = result
+-- arc consistency 1
+ac1 :: Net -> IO Net
+ac1 net@(Net ns cs) = result
     where
-        result = ns'
-        queue :: [Constraint a]
+        result = evalStateT reviser (net, queue)
         queue = concatMap (\c -> [c, flop c]) cs
-        ns' = evalStateT reviser (net, queue)
+        reviser :: StateT S IO Net
+        reviser = do
+            (net, cs) <- get
+            lift $ putStrLn "Next Iteration: "
+            lift $ print net
+            changes <- replicateM (length cs) checkSingleConstraint
+            (newnet, _) <- get
+            if any (==True) changes
+                then modify (\(n,_) -> (n,cs)) >> reviser
+                else lift (putStrLn "Finished with: ")
+                     >> lift (print newnet)
+                     >> return newnet
 
--- St a is the type used in the stateful computation.
-type S a = (Net a, [Constraint a])
-
--- implementing AC1
-reviser :: Show a => StateT (S a) IO (Net a)
-reviser = do
-    (net, cs) <- get
-    lift $ putStr "Iteration: "
-    lift $ print net
-    changes <- replicateM (length cs) checkSingleConstraint
-    (newnet, _) <- get
-    if any (==True) changes
-        then modify (\(n,_) -> (n,cs)) >> reviser
-        else return newnet
--}
 
 -- The State is the current net and a list of remaining
 -- to be validated constraints
 type S = (Net, [Constraint])
-    
+
 checkSingleConstraint :: StateT S IO Bool
 checkSingleConstraint = 
     do
         ((Net ns net_cs), cs) <- get
         case cs of
             [] -> return False
-            ((Binary _ s1 s2 f _):cs') ->
+            ((Binary cname s1 s2 f _):cs') ->
                 let n1 = fromJust $ find (\n -> name n == s1) ns
                     n2 = fromJust $ find (\n -> name n == s2) ns
                     (n1', changed) = f n1 n2
@@ -137,7 +135,7 @@ checkSingleConstraint =
                     newnet = Net ns' net_cs
                 in
                     when changed
-                        (lift (putStrLn $ s1 ++ " vs " ++ s2 ++ ": " ++ show n1 ++ " -> " ++ show n1'))
+                        (lift (putStrLn $ cname ++  ": " ++ show n1 ++ " -> " ++ show n1'))
                     >> put (newnet, cs')
                     >> return changed
 
@@ -151,6 +149,6 @@ replaceNode x y (b:bs)
 
 
 flop :: Constraint -> Constraint
-flop (Binary name s1 s2 c f) = mkConstraint s1 (flip f) s2 name
+flop (Binary name s1 s2 c f) = mkConstraint s2 (flip f) s1 name
 
 
--- Constraints/Demo.hs
+++ Constraints/Demo.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE FlexibleContexts, ConstraintKinds #-}
+{-# LANGUAGE FlexibleContexts, ConstraintKinds, DeriveDataTypeable #-}
 
 import ConstraintSolving
 
 nums :: Domain Int
 nums = [1,2,3,4]
+
+data Animal = Tiger | Gans | Katze deriving (Show, Eq, Typeable)
 
 net :: Net
 net = Net
@@ -11,13 +13,17 @@ net = Net
         var "V" nums,
         var "X" nums,
         var "Y" nums,
-        var "Z" ["1","2","3","4"]
+        var "A" [Tiger,Gans,Katze],
+        var "B" [Tiger,Gans,Katze],
+        var "Z" (map show nums)
     ]
     [
+       (mkConstraint :: On Animal) "A" (/=) "B" "A /= B",
        (mkConstraint :: On Int) "X" (<) "Y" "X < Y",
-       (mkConstraint :: Over Int String) "X" ((==) . show) "Z" "str(X) = Z"
+       (mkConstraint :: On Int) "Y" (\y v -> y +1 == v) "V" "Y +1 == V",
+       (mkConstraint :: Over Int String) "X" (\x z -> show (x+1) == z) "Z" "str(X+1) = Z"
     ]
 
-
+main = ac1 net >> return ()
 
 

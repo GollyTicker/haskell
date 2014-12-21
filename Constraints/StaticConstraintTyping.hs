96a49c9e30b0b60a242d8a2bda86630bc67aed54
GollyTicker <golly.ticker@gmail.com>
2014-12-21 16:24:55 +0100

    static

--- Constraints/StaticConstraintTyping.hs
+++ Constraints/StaticConstraintTyping.hs
@@ -1,27 +1,33 @@
-{-# LANGUAGE ScopedTypeVariables, FlexibleContexts, FlexibleInstances, ConstraintKinds, MultiParamTypeClasses, ExistentialQuantification, DeriveDataTypeable #-}
+{-# LANGUAGE ScopedTypeVariables,  FlexibleContexts, FlexibleInstances, ConstraintKinds, MultiParamTypeClasses, ExistentialQuantification, DeriveDataTypeable #-}
 
 import Data.Typeable
 
-data Net = Net [Node] deriving Show
+-- data Net c = Net [Node c] deriving Show
+data Net c where
+    Nil :: Net c
+    (+:) :: Net c -> Node c' -> (c,c')
 
 type Elem a = (Show a, Eq a, Typeable a)
 
-data Node =
-    forall a. (Elem a) => 
+-- c stands for context
+data Node c =
+    forall a. (Has c a, Elem a) => 
     Node String a
 
-instance Show Node where show (Node s a) = "Node " ++ show s ++ show a
+instance Show (Node c) where show (Node s a) = "Node " ++ show s ++ show a
 
 data Animal = A | B deriving (Show, Eq, Typeable)
 
-net = Net
-    [
-        Node "eins" A,
-        Node "zwei" (2::Int),
-        Node "drei" (B,'B'),
-        Node "eins" B,
-        Node "zwei" (undefined::Int)
-    ]
+data Void = Void !Void deriving (Show, Eq, Typeable)
+{-
+net :: forall c0 c1. Has c1 Animal => Net (c0,c1)
+net = n1
+    where
+        n0 = Net []
+        n1 = n0 +: (Node "eins" A)
+-}
+--(+:) :: Net c -> Node c' -> Net (c,c')
+--(Net ns) +: n = Net (n:ns)
 
 class Has c a where
     has :: c -> Maybe a
@@ -32,10 +38,18 @@ instance Has Int Int where
 instance (Has c a) => Has [c] a where
     has = foldr (const . has) Nothing
 
+instance (Has c a) => Has (c,b) a where
+    has = has . fst
+
+--instance (Has c a) => Has (b,c) a where
+--    has = has . snd
+
+hasAnimal :: Has ctx Animal => ctx -> Maybe Animal
+hasAnimal = has
 
 hasInt :: Has c Int => c -> Maybe Int
 hasInt = has
-    
+
 test1 = hasInt (3 :: Int)
 test2 = hasInt ([] :: [Int])
 test3 = hasInt ([1,2,3] :: [Int])

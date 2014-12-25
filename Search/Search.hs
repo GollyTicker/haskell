b7314a6b8e7c4ffd9e8a57f9d4656f06136b11cb
GollyTicker <golly.ticker@gmail.com>
2014-12-25 01:19:06 +0100

    tried converting to Set. giving up

--- Search/Search.hs
+++ Search/Search.hs
@@ -8,6 +8,17 @@ module Search (
     where
 
 -- TODO: Use Data.Sequence instead of Lists?
+-- TODO: Profiling first!
+-- TODO: Tests? xD
+
+{-
+
+Profiling:
+
+https://downloads.haskell.org/~ghc/5.04/docs/html/users_guide/profiling.html
+$ sudo apt-get install ghc-prof
+
+-}
 
 import Types
 import Utils
@@ -15,12 +26,24 @@ import Strategies
 
 search :: Problem a -> [Solution a]
 search p = search' p startNodes
-    where startNodes = [ [mkStartNode x] | x <- starts p ]
+    where startNodes = [ mkStartPath p x | x <- starts p ]
           
 expand :: Problem a -> Node a -> [Node a]
 expand pr (Node x _ _) = map toNode . concatMap (applyOn x) . actions $ pr
 
-search' :: Problem a -> [Path a] -> [Solution a]
+
+search' :: Problem a -> [SPath a] -> [Solution a]
+search' pr ps' | null ps' = []
+               | pr `checkGoalNode` tip = _getPath p : search' pr ps
+               | otherwise = let children = pr `expand` tip
+                                 new = mkNewPaths pr children p
+                                 all = insertNewPaths pr new ps
+                             in  search' pr all
+    where
+        (p:ps) = ps'
+        tip = head . _getPath $ p
+        
+{-
 
 search' pr (p@(tip:_):ps)
     | pr `checkGoalNode` tip = p : search' pr ps
@@ -33,9 +56,12 @@ search' pr (p@(tip:_):ps) =
 
 search' _  _              = []
 
-mkNewPaths :: Problem a -> [Node a] -> Path a -> [Path a]
-mkNewPaths pr ns p = [ n:p | n <- ns, (validChild . getElem) n ]
-    where validChild n = null $ filter (\n' -> n `eq` n') ns'
+-}
+
+mkNewPaths :: Problem a -> [Node a] -> SPath a -> [SPath a]
+mkNewPaths pr ns p = [ n `prepend` p | n <- ns, (validChild . getElem) n ]
+    where validChild n = cd || all (\n' -> not $ n `eq` n') ns'
+          cd = noCycleDetection pr
           eq = eqElem pr
           ns' = map getElem p
 
--- Search/Types.hs
+++ Search/Types.hs
@@ -1,8 +1,15 @@
-
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Types (
         module Types
     )
     where
+
+import qualified Data.Set as S
+import Data.Monoid
+import Data.Maybe
+
+(.:) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+f .: g = \x y -> f $ g x y
 
 data Strategy a =
     Depth
@@ -11,7 +18,7 @@ data Strategy a =
 --  | OptimistcClimbing
 --  | Custom (StrategyF a)
 
-type StrategyF a = [Path a] -> [Path a] -> [Path a]
+type StrategyF a = [SPath a] -> [SPath a] -> [SPath a]
 --                 NewPaths -> OldPaths -> AllPaths
 
 -- Problem a is a description of a problem where the search space
@@ -27,7 +34,25 @@ data Problem a =
         ,heuristic      :: Maybe (Heuristic a)
         ,actions        :: [Action a]
         ,strategy       :: Strategy a
+        ,ordering       :: Maybe (a -> a -> Ordering) -- if given, allows usage of Sets to optimize cycle detection
+        ,noCycleDetection :: Bool
+            -- enable, if cycles are impossible in your domain.
+            -- reduces time comsumption
         -- add custom strategies here?
+    }
+
+-- default Problem for easier use with record Syntax
+mkProblem :: Problem a
+mkProblem = Problem {
+         starts = error "mkProblem{starts = ... Please specify ... }: "
+        ,showElem = error "mkProblem{showElem = ... Please specify ... }: "
+        ,checkGoal = error "mkProblem{checkGoal = ... Please specify ... }: "
+        ,eqElem = error "mkProblem{eqElem = ... Please specify ... }: "
+        ,heuristic = Nothing
+        ,actions = error "mkProblem{actions = ... Please specify ... }: "
+        ,strategy = Breadth
+        ,noCycleDetection = False
+        ,ordering = Nothing
     }
 
 checkGoalNode :: Problem a -> Node a -> Bool
@@ -41,8 +66,10 @@ data Node a =
     }
 
 
-mkStartNode :: a -> Node a
-mkStartNode x = Node x Start Nothing
+mkStartPath :: Problem a -> a -> SPath a
+mkStartPath pr x = case ordering pr of 
+        Nothing -> SPath $ (,) [Node x Start Nothing] S.empty
+        Just f  -> SPath $ (,) [Node x Start Nothing] (fromList f [x])
 
 toNode :: AppliedAction a -> Node a
 toNode aa@(AA x _ _ _) = Node x aa Nothing
@@ -67,6 +94,44 @@ getPath = map getElem
 
 -- a Path is a list of Nodes starting with the latest one.
 type Path a = [Node a]
+
+-- a SPath also has a set of the nodes. easier for equality checking
+newtype SPath a = SPath ( Path a, S.Set (Order a) ) deriving Monoid
+
+_getPath :: SPath a -> Path a
+_getPath (SPath (x,_)) = x
+
+getElems :: Path a -> [a]
+getElems = map getElem
+
+-- applies a reordering function over Paths to SPaths.
+-- its like a functor from Paths to SPaths
+reorderPaths :: Problem a -> ( [Path a] -> [Path a] ) -> ( [SPath a] -> [SPath a] )
+reorderPaths pr f xs = let ys  = f $ map _getPath xs
+                       in  case ordering pr of
+                            Just ord -> 
+                                    let ys' = map (fromList ord . getElems) ys
+                                    in  zipWith (SPath .: (,)) ys ys'
+                            Nothing -> zip
+
+fromList :: (a -> a -> Ordering) -> [a] -> S.Set (Order a)
+fromList f = S.fromList . map (`Order` f)
+
+prepend :: Node a -> SPath a -> SPath a
+prepend x (SPath (xs,xs')) = SPath $ (,) (x:xs) (S.insert x xs')
+
+overPaths :: ([Path a] -> b) -> [SPath a] -> b
+overPaths f = \xs -> f (map _getPath xs)
+
+-- since a might not be instance of Ord, but we are given an implementation
+-- we create a datastructure here solely for implementing the typeclass
+-- to use in S.Set
+data Order a = Order a (a -> a -> Ordering)
+
+instance Ord (Order a) where
+    compare (Order a f) (Order b _) = f a b
+
+instance Eq (Order a) where (Order a f) == (Order b _) = f a b == EQ
 
 type Heuristic a = a -> HValue
 

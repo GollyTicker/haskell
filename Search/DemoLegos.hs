c81991508c206f8e54cd36f7533d69abdd34f288
GollyTicker <golly.ticker@gmail.com>
2014-12-28 00:25:18 +0100

    stuff

--- Search/DemoLegos.hs
+++ Search/DemoLegos.hs
@@ -1,38 +1,55 @@
-
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
 import Search
 
 import qualified Data.Set as S
 import Data.List
 import Data.Ord
+import Data.Maybe
+import Text.Printf
+
+
+main = do
+    putStrLn "Are there errors in the initial world?"
+    print $ validWorld initialWorld
+    putStrLn "Initial world:"
+    putStrLn $ topview initialWorld
 
 type P = SPath
 type Size = Int
 type Pos = (Int,Int,Int)
 type Legos = S.Set (Posed Lego)
+
 data World = 
     World {
-         holding    :: Maybe (Posed Lego) -- lego currently in hand
+         holding    :: Maybe Lego -- lego currently in hand
         ,worldSize  :: Size     -- the world is a cube with Size length,widht and height
         ,legos      :: S.Set (Posed Lego)
     } deriving (Ord, Eq, Show)
 
-data Lego = L2x6x2 | L6x2x2 | L2x2x2 deriving (Show, Eq, Ord, Read, Bounded)
+data Lego = 
+    L6x2x2   | L2x6x2 
+    | L4x2x2 | L2x4x2
+    | L2x2x2
+    deriving (Show, Eq, Ord, Read, Bounded)
 
 legoBox :: Lego -> Vector
 legoBox L2x6x2 = minusOne $ Vector (2,6,2) -- converting to zero based
 legoBox L6x2x2 = minusOne $ Vector (6,2,2)
 legoBox L2x2x2 = minusOne $ Vector (2,2,2)
+legoBox L4x2x2 = minusOne $ Vector (4,2,2)
+legoBox L2x4x2 = minusOne $ Vector (2,4,2)
 
 minusOne :: Vector -> Vector
 minusOne (Vector (x,y,z)) = Vector (x-1,y-1,z-1)
 
-data Posed a = PS { __size :: Size, getLego :: Lego, getOrigin :: Pos, getEdge :: Pos} deriving Show
+data Posed a = PS { getLego :: a, getOrigin :: Pos, getEdge :: Pos} deriving Show
 -- Pos is the left-bottom Coordinate of the Lego Block
--- Size is the size of the world.  it could be used to rotate blocks
--- in the higher and middle part to the upper left corner for comparision
+-- a size attribute saving the worlds size could be used to rotate blocks
+-- in the higher and middle part to the upper left corner for meaningful
+-- interpretations for findMaximum/minimum and lookupGT/LT in the Set
 
 getPBox :: Posed Lego -> PBox
-getPBox (PS _ _ a b) = (a,b)
+getPBox (PS _ a b) = (a,b)
 
 instance Ord (Posed a) where compare = comparing getOrigin
 -- no need to compare lego, because that would mean that the two Lego are overlapping
@@ -42,9 +59,9 @@ instance Eq (Posed a) where x == y = (EQ==)$ comparing getOrigin x y
 legoProblem :: Problem P World
 legoProblem =
     mkProblem {
-         starts      = [ beginning ]
+         starts      = [ initialWorld ]
         ,checkGoal   = allOnFloor
-        ,showElem    = show
+        ,showElem    = topview -- show
         ,eqElem      = (==)
 
         ,actions     = [
@@ -57,30 +74,73 @@ legoProblem =
     }
 
 allOnFloor :: World -> Bool
-allOnFloor = all (\(x,y,z) -> z == 0) . map getOrigin . S.toList . legos
+allOnFloor = all onFloor . S.toList . legos
+
+onFloor :: Posed Lego -> Bool
+onFloor = uncurry3 (\x y z -> z == 0) . getOrigin
 
 doStuff :: World -> [World]
 doStuff (World Nothing s ls) = [ World (Just l) s ls' | (l,ls') <- pickable ls ]
-    where
-        pickable :: Legos -> [(Posed Lego, Legos)]
-        pickable set = [ (p,set') | p <- picks, let set' = S.delete p set ]
-            where
-                picks = S.toList $ S.filter nothingAbove set
-                nothingAbove posl = surfaceFree set $ boxTop $ getPBox posl
+doStuff (World (Just l) s ls) = [ World Nothing s ls' | ls' <- putable l ls ]
 
-doStuff (World (Just l) s ls)  = undefined
-    -- find all places one can put this lego at
+-- find all places one can put this lego at
+putable :: Lego -> Legos -> [Legos]
+putable l ls = [ S.insert l' ls | l' <- newPositions ]
+    where
+        surfaces     :: [Pos]
+        newPositions :: [Posed Lego]
+        surfaces = undefined
+        newPositions =
+            filter (isFree ls . getPBox)
+            . map (uncurry3 (mkPositionedLego l))
+            $ surfaces
+--        surfaces = undefined
+  --      validOrigins = map undefined -- check for collisions, surfaces
+
+uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
+uncurry3 f = \(a,b,c) -> f a b c
+
+-- find all legos one can pick
+pickable :: Legos -> [(Lego, Legos)]
+pickable set = [ (lego,set') | p@(PS lego _ _) <- picks, let set' = S.delete p set ]
+    where
+        picks = S.toList $ S.filter nothingAbove set
+        nothingAbove posl =
+            isFree set
+            $ moveUp
+            $ boxTop
+            $ getPBox posl
+
 
 type PBox = (Pos, Pos) -- a box anywhere
 
 newtype Vector = Vector Pos -- a Vector/Box from the origin
 
-surfaceFree :: Legos -> PBox -> Bool
-surfaceFree set b = S.null $ intersectsL set b
+-- a small typeclass to refactor a few functions with similar usages
+class Vectorspace a where
+    move        :: Vector -> a -> a
+    moveUp      :: a -> a
+    moveUp = move (Vector (0,0,1))
+    moveDown    :: a -> a
+    moveDown = move (Vector (0,0,-1))
+    -- should satisfy 
+    --     move (Vector (0,0,0)) = id
 
-intersectsL :: Legos -> PBox -> Legos
-intersectsL set x = S.filter (intersects x . getPBox) set
+instance Vectorspace Pos where
+    move (Vector (dx,dy,dz)) (x,y,z) = (x + dx, y + dy, z + dz)
 
+instance Vectorspace PBox where
+    move v (p,p') = (move v p, move v p')
+
+-- is the box free in the set of legos?
+isFree :: Legos -> PBox -> Bool
+isFree set b = S.null $ intersectsPBox b set
+
+-- which legos intersect with with this box?
+intersectsPBox :: PBox -> Legos -> Legos
+intersectsPBox b = S.filter (intersects b . getPBox)
+
+-- are two boxes intersecting?
 intersects :: PBox -> PBox -> Bool
 intersects a b =
     let ( (ax,ay,az), (ax',ay',az') ) = a
@@ -93,33 +153,103 @@ intersects a b =
 rint :: Int -> Int -> Int -> Int -> Bool
 rint x x' y y' = x <= y' && y <= x'
 
+-- what is the top plane of the box?
 boxTop :: PBox -> PBox
-boxTop ((x,y,_),(x',y',z')) = ((x,y,z'), (x,y,z'))
+boxTop ((x,y,_),(x',y',z')) = ((x,y,z'), (x',y',z'))
 
---
-isValidworld :: World -> IO ()
-isValidworld = undefined
--- check all legos for intersects and check that no lego is flying
+-- what is the bottom plane of the box?
+boxBot :: PBox -> PBox
+boxBot ((x,y,z),(x',y',_)) = ((x,y,z), (x',y',z))
 
-beginning :: World
-beginning = 
+
+-- check all legos for intersects and ensure that no lego is flying
+-- returns a Just of errors if invalid
+validWorld :: World -> Maybe ( Legos, [(Posed Lego,Posed Lego)] )
+validWorld w | S.null flys && null intersects = Nothing
+             | otherwise = Just (flys, intersects)
+    where
+        intersects = intersections ls
+        ls = legos w
+        flys = flyings ls
+
+-- which legos are flying?
+flyings :: Legos -> Legos
+flyings ls = S.filter flying ls
+    where
+        flying lego = let pb = getPBox lego
+                          floor = boxBot pb
+                          underneath = moveDown floor
+                          legosUnderneath = intersectsPBox underneath ls
+                      in  not (onFloor lego) && S.null legosUnderneath
+
+-- which legos are intersecting?
+intersections :: Legos -> [(Posed Lego,Posed Lego)]
+intersections = catMaybes . forPairs f . (S.toList)
+    where
+        f a b | intersects (getPBox a) (getPBox b) = Just (a,b)
+              | otherwise = Nothing
+
+forPairs :: Eq a => (a -> a -> c) -> [a] -> [c]
+forPairs f as = [ f a a' | a <- as, a' <- as, a /= a' ]
+
+initialWorld :: World
+initialWorld = 
     let size = 10
     in  World Nothing size
         $ S.fromList
-        $ zipWith4 (mkPositionedLego size)
-            [ L2x2x2 ,L2x2x2 ,L2x6x2 ,L6x2x2 ]
-            [ 0      ,2      ,0      ,3      ]
-            [ 0      ,0      ,0      ,3      ]
-            [ 0      ,0      ,2      ,2      ]
+        $ zipWith4 mkPositionedLego
+            [ L2x2x2 ,L2x2x2 ,L2x6x2 ,L6x2x2 ,L2x4x2 ,L4x2x2]
+            [ 0      ,2      ,0      ,1      ,6      ,2     ]
+            [ 0      ,0      ,0      ,4      ,3      ,5     ]
+            [ 0      ,0      ,2      ,4      ,0      ,6     ]
 
-mkPositionedLego :: Int -> Lego -> Int -> Int -> Int -> Posed Lego
-mkPositionedLego size l x y z = 
+-- How the world looks like
+-- Topview. The number says the height
+{-
+    0    x   9
+  0 3311000000
+    3311000000
+    3300000000
+    3300001100
+  y 3555555100
+    3577775100
+    0077771100
+    0000000000
+    0000000000
+  9 0000000000
+-}
+
+topview :: World -> String
+topview (World h s ls) = "Holding " ++ show h ++ "\n" ++ topview' s ls
+
+topview' :: Int -> Legos -> String
+topview' s ls = toString heights
+    where
+        range = [0..(s-1)]
+        heights :: [[Int]]
+        heights = [ [ heightAt x y | x <- range] | y <- range]
+        heightAt :: Int -> Int -> Int
+        heightAt x y = 
+            let rod = ((x,y,0),(x,y,s))
+                onRod = intersectsPBox rod ls
+            in  if S.null onRod
+                    then 0
+                    else S.findMax . S.map (getZ . snd . getPBox) $ onRod
+
+toString :: Show a => [[a]] -> String
+toString = intercalate "\n" . map (intercalate "". map show)
+
+getX :: (a,b,c) -> a
+getX (a,b,c) = a
+getY :: (a,b,c) -> b
+getY (a,b,c) = b
+getZ :: (a,b,c) -> c
+getZ (a,b,c) = c
+
+mkPositionedLego :: Lego -> Int -> Int -> Int -> Posed Lego
+mkPositionedLego l x y z = 
     let origin = (x,y,z)
-    in  PS size l origin (move (legoBox l) origin)
-
-move :: Vector -> Pos -> Pos
-move (Vector (dx,dy,dz)) (x,y,z) = (x + dx, y + dy, z + dz)
-
+    in  PS l origin (move (legoBox l) origin)
 
 -- alternatively model the world as a tree of legos
 -- where every lego points to its immediate lower ones.

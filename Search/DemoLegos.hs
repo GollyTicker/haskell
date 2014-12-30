cb927a3e01228876059a8d39d9369d99208aa91b
GollyTicker <golly.ticker@gmail.com>
2014-12-30 23:04:03 +0100

    superb. added Arbitrary Instance for World. added a few properties. all properties satisfied. finished puting and picking. maybe search can beging now... xD

--- Search/DemoLegos.hs
+++ Search/DemoLegos.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances, TemplateHaskell, FlexibleInstances #-}
 import Search
 
 import qualified Data.Set as S
@@ -7,6 +7,11 @@ import Data.Ord
 import Data.Maybe
 import Text.Printf
 import Debug.Trace
+import Test.QuickCheck
+import System.Random
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
 
 import Control.Monad.Identity
 import Control.Applicative
@@ -38,7 +43,7 @@ using the provided function at the left."
 linebreak :: IO ()
 linebreak = putStrLn "----------------"
 
-main:: IO ()
+main :: IO ()
 main = do
     linebreak
     
@@ -56,10 +61,13 @@ main = do
     showTop x
     linebreak
     
-    let ys = put x -- TODO...
+    let ys = put x
     printf "Showing first of %d possibilities to put down\n" (length ys)
     showTop (head ys)
     linebreak
+    
+    putStrLn "Running Tests"
+    void runTests
 
 showTop :: World -> IO ()
 showTop = putStrLn . topview
@@ -88,13 +96,26 @@ data World =
          holding    :: Maybe Lego -- lego currently in hand
         ,worldSize  :: Size     -- the world is a cube with Size length,widht and height
         ,legos      :: S.Set (Posed Lego)
-    } deriving (Ord, Eq, Show)
+    } deriving (Ord, Eq)
+instance Show World where show = topview
 
 data Lego = 
     L6x2x2   | L2x6x2 
     | L4x2x2 | L2x4x2
     | L2x2x2
-    deriving (Show, Eq, Ord, Read, Bounded)
+    deriving (Show, Eq, Ord, Read, Enum, Bounded)
+
+instance Random Lego where
+    randomR (a,b) g =
+        let domain = [a .. b] :: [Lego]
+            len = length domain
+            (i,g') = next g
+            i' = i `mod` len
+        in  (domain !! i', g')
+    random = randomR (minBound,maxBound)
+
+instance Arbitrary Lego where
+    arbitrary = choose (minBound, maxBound)
 
 legoBox :: Lego -> Vector
 legoBox L2x6x2 = minusOne $ Vector (2,6,2) -- converting to zero based
@@ -114,6 +135,9 @@ data Posed a = PS { getLego :: a, getOrigin :: Pos, getEdge :: Pos} deriving Sho
 
 getBox :: Posed Lego -> Box
 getBox (PS _ a b) = (a,b)
+
+count :: World -> Int
+count (World ml _ set) = maybe 0 (const 1) ml + S.size set
 
 instance Ord (Posed a) where compare = comparing getOrigin
 -- no need to compare lego, because that would mean that the two Lego are overlapping
@@ -148,11 +172,12 @@ onFloor = uncurry3 (\x y z -> z == 0) . getOrigin
 -- voneinander definieren...
 
 pick :: World -> [World]
-pick (World Nothing size s) = [ validate $ World (Just l) size s' | (l,s') <- pickable s ]
+pick (World Nothing size s) = [ World (Just l) size s' | (l,s') <- pickable s ]
 pick _ = []
 
 put :: World -> [World]
-put (World (Just l) size s) = [ validate $ World Nothing size s' | s' <- putable size l s ]
+put (World (Just l) size s) =
+    [ w | s' <- putable size l s, let w = World Nothing size s', isValidWorld w]
 put _ = []
 
 -- for debug
@@ -164,10 +189,25 @@ validate w | validWorld w == Nothing     = w
 putable :: Size -> Lego -> Legos -> [Legos]
 putable size l s = [ S.insert l' s | l' <- newPositions ]
     where
-        newPositions = filter (isLoose s) $ mkLego <$> range <*> range <*> range  
+        newPositions = filter eligible $ mkLego <$> range <*> range <*> range  
         range = [0..(size-1)]
+        eligible = ((&&) .: (&&)) <$> collisionFree s <*> isLoose s <*> inWorld size
         mkLego :: Int -> Int -> Int -> Posed Lego
         mkLego = mkPositionedLego l
+
+
+
+inWorld :: Size -> Posed Lego -> Bool
+inWorld size = and6 <$> inrange . getX . fst . getBox
+                    <*> inrange . getY . fst . getBox
+                    <*> inrange . getZ . fst . getBox
+                    <*> inrange . getX . snd . getBox
+                    <*> inrange . getY . snd . getBox
+                    <*> inrange . getZ . snd . getBox
+    where
+        and6 a b c d e f = a && b && c && d && e && f
+        inrange = (`elem` range)
+        range = [0..(size-1)]
 
 uncurry3 :: (a -> b -> c -> d) -> (a,b,c) -> d
 uncurry3 f = \(a,b,c) -> f a b c
@@ -219,6 +259,8 @@ boxTop ((x,y,_),(x',y',z')) = ((x,y,z'), (x',y',z'))
 boxBot :: Box -> Box
 boxBot ((x,y,z),(x',y',_)) = ((x,y,z), (x',y',z))
 
+isValidWorld :: World -> Bool
+isValidWorld = not . isJust . validWorld
 
 -- check all legos for intersects and ensure that no lego is flying
 -- returns a Just of errors if invalid
@@ -241,6 +283,9 @@ topFree, botFree, boxFree :: Legos -> Posed Lego -> Bool
 topFree set = isFree set . moveUp   . boxTop . getBox
 botFree set = isFree set . moveDown . boxBot . getBox
 boxFree set = isFree set . getBox -- is the box of this lego free in the given set?
+
+collisionFree :: Legos -> Posed Lego -> Bool
+collisionFree s l = S.null $ intersectsBox (getBox l) s
 
 -- which legos are intersecting?
 intersections :: Legos -> [(Posed Lego,Posed Lego)]
@@ -314,3 +359,69 @@ mkPositionedLego l x y z =
 -- alternatively model the world as a tree of legos
 -- where every lego points to its immediate lower ones.
 -- this makes finding free legos much easier
+
+
+simpleWorld = 
+    let size = 6
+    in  World (Just L2x2x2) size
+        $ S.fromList
+        $ zipWith4 mkPositionedLego
+            [ L2x2x2, L2x2x2, L2x2x2 ]
+            [ 0     , 0     , 0      ]
+            [ 0     , 1     , 2      ]
+            [ 0     , 2     , 4      ]
+
+main2 = do
+    showTop simpleWorld >> print simpleWorld
+    linebreak >> linebreak
+    let xs = put simpleWorld
+    mapM_ (\x -> showTop x >> print x >> linebreak) xs
+
+
+showSample :: IO ()
+showSample = sample' (arbitrary :: Gen World) >>= mapM_ showTop
+
+instance Arbitrary World where
+    arbitrary = do
+            size           <- choose (2,8)
+            (World _ _ s) <- repeatAWhile 5 (World Nothing size S.empty)
+            ml             <- arbitrary :: Gen (Maybe Lego)
+            return (World ml size s)
+        where
+            repeatAWhile n (World _ size ls) = do
+                    i <- arbitrary :: Gen Int
+                    l      <- arbitrary :: Gen Lego
+                    let w = (World (Just l) size ls)
+                        xs = put w
+                    let stop = minBound + (round $ (0.5 :: Double) * fromIntegral (maxBound - minBound :: Int) )
+                    if n == 0 || i < stop || null xs then return w else elements xs >>= repeatAWhile (n-1)
+
+_expand w = put w ++ pick w
+
+-- the the number of legos doesn't change on expansions
+prop_PutPickCount = \w -> and $ map ((==EQ) . comparing count w) (_expand w)
+
+-- all expansions should be valid Worlds
+prop_ValidGen = all isValidWorld . _expand
+
+-- In a World with exactly one Lego, that Lego is always pickable.
+prop_SinglePickable = \w -> ((1==) . S.size . legos) w ==> isLoose (legos w) (S.elemAt 0 $ legos w)
+
+-- After Puting a Lego, at least one Lego is Loose.
+prop_LooseAfterPut =
+    \w -> let xs = put w
+          in  all (\w' -> let s = legos w' in not . S.null . S.filter (isLoose s) $ s) xs
+
+-- consecutive put/pick actions may reverse each other.
+prop_PutPickMayReverse = \w ->
+    let as = _expand w
+        xs = concatMap _expand as
+    in  not (null xs) ==> w `elem` xs
+
+-- how does the number of loose legos change on put/pick?
+-- prop_PutPickLoose = \w -> and $ map ((==EQ) . comparing (S.size . legos) w) (_expand w)
+
+runTests = $quickCheckAll
+rt = runTests
+
+
